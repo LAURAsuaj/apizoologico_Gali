7f95e166e8c3dc22a196b0fc0c48b98558269035
juan <juanito@gmail.com>
2025-04-01 16:18:52 -0500

    Modificar el nombre de un animal

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -14,5 +14,17 @@ router.get("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Modificar el nombre de un animal por su id
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
 
 module.exports = router;
